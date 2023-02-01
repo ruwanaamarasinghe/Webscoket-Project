0252b1721a9943eed0d187cf0f0437307dfc0792
Ruwana Amarasinghe <ruwanaamarasinghe@gmail.com>
2023-02-01 14:34:08 +0530

    create index.js

---
+++ index.js
@@ -0,0 +1,121 @@
+const http = require("http");
+const app = require("express")();
+app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"))
+app.listen(9091, ()=>console.log("Listening on http port 9091"))
+const websocketServer = require("websocket").server
+const httpServer = http.createServer();
+httpServer.listen(9090, () => console.log("Listening.. on 9090"))
+//hashmap clients
+const clients = {};
+const games = {};
+
+const wsServer = new websocketServer({
+    "httpServer": httpServer
+})
+wsServer.on("request", request => {
+    //connect
+    const connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("opened!"))
+    connection.on("close", () => console.log("closed!"))
+    connection.on("message", message => {
+        const result = JSON.parse(message.utf8Data)
+        if (result.method === "create") {
+            const clientId = result.clientId;
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "balls": 20,
+                "clients": []
+            }
+
+            const payLoad = {
+                "method": "create",
+                "game" : games[gameId]
+            }
+
+            const con = clients[clientId].connection;
+            con.send(JSON.stringify(payLoad));
+        }
+
+        //a client want to join
+        if (result.method === "join") {
+
+            const clientId = result.clientId;
+            const gameId = result.gameId;
+            const game = games[gameId];
+            if (game.clients.length >= 2) 
+            {
+                //sorry max players reach
+                return;
+            }
+            const color =  {"0": "#F48484", "1": "#84D2C5"}[game.clients.length]
+            game.clients.push({
+                "clientId": clientId,
+                "color": color
+            })
+            //start the game
+            if (game.clients.length === 2) updateGameState();
+
+            const payLoad = {
+                "method": "join",
+                "game": game
+            }
+            //loop through all clients and tell them that people has joined
+            game.clients.forEach(c => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad))
+            })
+        }
+        //a user plays
+        if (result.method === "play") {
+            const gameId = result.gameId;
+            const ballId = result.ballId;
+            const color = result.color;
+            let state = games[gameId].state;
+            if (!state)
+                state = {}
+            
+            state[ballId] = color;
+            games[gameId].state = state;
+            
+        }
+
+    })
+
+    //generate a new clientId
+    const clientId = guid();
+    clients[clientId] = {
+        "connection":  connection
+    }
+
+    const payLoad = {
+        "method": "connect",
+        "clientId": clientId
+    }
+    connection.send(JSON.stringify(payLoad))
+
+})
+function updateGameState(){
+
+    //{"gameid", fasdfsf}
+    for (const g of Object.keys(games)) {
+        const game = games[g]
+        const payLoad = {
+            "method": "update",
+            "game": game
+        }
+
+        game.clients.forEach(c=> {
+            clients[c.clientId].connection.send(JSON.stringify(payLoad))
+        })
+    }
+
+    setTimeout(updateGameState, 500);
+}
+
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+ 
+// then to call it, plus stitch in '4' in the third group
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+ 
